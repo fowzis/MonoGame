3222ebc9f0f4e00d639994afa1f82c97a238594b
Fowzi Shakkour <fowzis@hotmail.com>
2018-01-20 10:39:20 +0200

    Changed namespace name

--- HelperUtilities/Classes/PropertyHelper.cs
+++ HelperUtilities/Classes/PropertyHelper.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 
-namespace HelperUtilsLib
+namespace Shakkour.Utils
 {
     public static class PropertyHelper
     {
